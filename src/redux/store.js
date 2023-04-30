3107a4975cb68d4b19be0876153304b9771c3f3f
Olga Lorenz <ol900ivas@gmail.com>
2023-04-30 09:06:05 +0300

    add blacklist

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,6 +15,7 @@ import { rootReducer } from './reducer';
 const persistConfig = {
   key: 'contacts',
   storage,
+  blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
